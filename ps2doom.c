bf33950615bcb5858f6cfee8daa671beaae8118e
Adilson Enio Pierog <dirsors@gmail.com>
2022-12-23 23:08:00 -0300

    Update ps2doom.c

--- ps2doom.c
+++ ps2doom.c
@@ -1,5 +1,5 @@
 #include <tamtypes.h>
-#include <floatlib.h>
+#include <math.h>
 #include <stdio.h>
 
 int gethostname(char *name, int len)
@@ -10,16 +10,5 @@ int gethostname(char *name, int len)
 
 u32 inet_addr(const char *cp)
 {
-	return 0;
-}
-
-float pow(float a, float b)
-{
-	return powf(a,b);
-}
-
-int setbuf ( FILE * stream, char * buffer )
-{
-
 	return 0;
 }
